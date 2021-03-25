396d88a8466353a6021590079d8305596cb0df00
NiteshRajput1999 <nitesh@zilliontechnologies.com>
2021-03-25 16:45:50 +0530

    Add project files.

---
+++ AzureIntegrator/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AzureIntegrator.Models;
+
+namespace AzureIntegrator.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            var login = new Account() ;
+            return View("~/Views/Account/Login/Login.cshtml");
+        }
+    }
+}
---
+++ AzureIntegrator/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureIntegrator.Models
+{
+    public class Account
+    {
+        public string Uid { get; set; }
+        public string Password { get; set; }
+    }
+}
